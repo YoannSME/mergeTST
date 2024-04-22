f72f1bd437d94b70ff52bdd18560597cf7576e84
YoannSME <yoann.smets@univ-tlse3.fr>
2024-04-22 08:32:21 +0200

    b

---
+++ .metadata/.plugins/org.eclipse.core.resources/.projects/tst/org.sonarlint.eclipse.core/issues/index.pb
@@ -0,0 +1,3 @@
+
+B
+src/tst/test1.java,7\d\7de766c6810f7e99edc971d9a94941f13a5f2f73
---
+++ tst/src/tst/tst2.java
@@ -0,0 +1,8 @@
+package tst;
+
+public class tst2 {
+	public static void main(String[] args) {
+		System.out.println("aaa");
+	}
+
+}
